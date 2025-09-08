d8d1ea25ff5fd686f53e12b41d4b5f98a35e23c9
sthomasen7 <sthomasen7@gmail.com>
2025-09-07 21:43:24 -0600

    create the skeleton of the maze class

--- src/maze.py
+++ src/maze.py
@@ -1,7 +1,42 @@
 #!/home/stasen/anaconda3/envs/atms_env/bin/python
+import os
 
-def maze():
-    print("Hello world!")
+def clear_screen():
+    pass
+
+class Maze():
+
+    def __init__(self):
+        self.maze_size = 8
+        self.maze = list()
+        for i in range(self.maze_size):
+            self.maze.append([0]*self.maze_size)
+
+    def game_loop(self):
+        while not should_exit():
+            pass
+
+    def should_exit(self):
+        return False
+
+    def get_input(self):
+        return False
+
+    def move(self, direction):
+        pass
+
+    def draw_screen(self):
+        """draw the maze"""
+        for i in range(self.maze_size):
+            for j in range(self.maze_size):
+                print(self.maze[i][j], end="")
+            print("")
+
+    def _generate(self):
+        pass
+
 
 if __name__ == "__main__":
-    maze()
+    maze = Maze()
+    maze.game_loop()
+
